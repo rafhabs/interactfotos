ee508dd6485d07259814d79a1e1f49f7c751e172
rafhabs <rafhabs@gmail.com>
2021-05-02 12:54:33 -0300

    main activy em desenvolvimento

--- app/src/main/java/com/rafhabs/interactfotos/MainActivity.kt
+++ app/src/main/java/com/rafhabs/interactfotos/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.rafhabs.interactfotos
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pick_button.setOnClickListener{
+            // conferindo se a versão do usuário é superior a configuração do APP
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // confere se a permissão está negada
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    //faz a requisição da permissão
+                    val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE)
+                } else {
+                    pickImageFromGallery()
+                }
+            } else {
+                pickImageFromGallery()
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+
+            PERMISSION_CODE -> {
+                // conferindo a permissão autorizada
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery()
+                } else
+                {
+                    // envia alerta na tela para o usuário
+                    Toast.makeText(this,"Permissão negada",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+    }
+
+    // bloco de valores obrigatorios nos metodos
+    companion object {
+        private val PERMISSION_CODE = 1000
+        private val IMAGE_PICK_CODE = 1001
     }
 }
